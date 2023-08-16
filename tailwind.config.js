db755740191a39213fa9821901235ca77e4f770e
Skrwgrgle <kenpachiarts@gmail.com>
2023-08-16 18:52:08 +0700

    main

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        colorText: "#f8fafc",
+        colorBg: "#2A292D",
+      },
+    },
+  },
+  plugins: [],
+};
