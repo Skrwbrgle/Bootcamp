2e8433f32822f5fc09ba034dacbd3b6b392cab4c
Skrwbrgle <kenpachiarts@gmail.com>
2023-08-20 20:25:38 +0700

    up baru

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,24 +1,23 @@
 //navbar fiexd
-window.onscroll = function() {
-    const header = document.querySelector('header');
-    const scroll = document.querySelector('#sticky');
-    const fixedNav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const scroll = document.querySelector("#sticky");
+  const fixedNav = header.offsetTop;
 
-    if(window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-        // scroll.classList.remove('scroll-nav');
-    } else {
-        header.classList.remove('navbar-fixed');
-        // scroll.classList.add('scroll-nav');
-        
-    }
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    scroll.classList.remove("scroll-nav");
+  } else {
+    header.classList.remove("navbar-fixed");
+    scroll.classList.add("scroll-nav");
+  }
 };
 
 //hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu =document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
